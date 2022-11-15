406c136e5e5be11a767058b65ee64623184e9f1b
Aviv <acohn@dgitech.com>
2022-11-15 14:16:20 -0500

    Adding cs file extensions, adding length of last word

---
+++ LeetCode/Easy/LengthofLastWord.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution 
+{
+
+    public static void Main()
+    {
+        Console.WriteLine(LengthOfLastWord("luffy is still joyboy"));
+    }
+    public static int LengthOfLastWord(string s)
+    {
+        List<string> Words = s.Split(' ').ToList();
+        Words.RemoveAll(x => x == "");
+        return Words[Words.Count - 1].Length;
+
+
+    }
+}
