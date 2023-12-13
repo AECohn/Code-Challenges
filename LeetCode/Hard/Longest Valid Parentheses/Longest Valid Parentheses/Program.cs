3d2fc8636256031ddf825fbc216d7a9067d84b43
Aviv Cohn <acohn@dgitech.com>
2023-12-13 16:25:25 -0500

    Misc

---
+++ LeetCode/Hard/Longest Valid Parentheses/Longest Valid Parentheses/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution 
+{
+    public int LongestValidParentheses(string s)
+    {
+        Stack<char> Matcher = new Stack<char>();
+        foreach (char character in s)
+        {
+            if (character == '(')
+            {
+                
+            }
+        }
+    }
+}
